80d18dcdb9a0e5eb68a45976c917119ee79dc1c6
pikymga <eugeniarossanigo@gmail.com>
2021-05-03 21:44:00 -0300

    final test added

--- test/specs/wiki-automation.js
+++ test/specs/wiki-automation.js
@@ -1,50 +1,18 @@
-describe('Test', () => {
-    it('open browser', () => {
-        browser.url('https://google.com');
+describe('Get wikipedia cover', () => {
+    beforeAll('open browser', () => {
+        return browser.url('https://google.com/');
+    });
+    
+    it('To wikipedia', () => {
+        let inputBox = $('.gLFyf');
+        expect(inputBox).toExist();
+        inputBox.setValue('wikipedia');
+        browser.keys('Enter');
+        let inputUrl = $('.LC20lb');
+        //ver $(selector).getText()
+        inputUrl.click();
         browser.pause(4000);
-    })
+        expect(browser).toHaveUrl('https://es.wikipedia.org/wiki/Wikipedia:Portada');
+        expect(browser).toHaveTitleContaining('Wikipedia');
+    });
 })
-
-
-// describe('Get wikipedia cover', () => {
-//     beforeAll('open browser', () => {
-//         return browser.url('https://google.com');
-//     });
-    
-//     it('To wikipedia', () => {
-//         let inputBox = $('#gLFyf gsfi');
-//         expect(inputBox).toExist();
-//         inputBox.setValue('wikipedia');
-//         browser.keys('Enter');
-//         let inputUrl = $('.LC20lb DKV0Md=Wikipedia');  // como selecciono la primera??? Ver si funciona
-//         //ver $(selector).getText()
-//         inputUrl.click();
-//         browser.pause(4000);
-
-//         //expect(browser.getTitle()).toMatch('https://es.wikipedia.org/wiki/Wikipedia:Portada');
-//         expect(browser).toHaveUrl('https://es.wikipedia.org/wiki/Wikipedia:Portada');
-//         expect(browser).toHaveTitleContaining('Wikipedia');
-//     });
-// })
-
-
-//EJEMPLO 
-// describe('Google', () => {
-//     it('Title', () => {
-//         browser.url('https://wikipedia.org');
-//         browser.pause(2000);
-//         expect(browser).toHaveTitle('Google');
-//     });
-    
-//     beforeAll('open browser', () => {
-//         return browser.url('https://google.com');
-//     });
-//     it('can handle commands sync',() => {
-//         let value = $('#1b');  // id de google
-//         expect(value).toExist(); // si el selector existe pasa
-//     });
-//     it('title', () => {
-//         browser.pause(2000); // pausa
-//         expect(browser.getTitle()).toMatch('Google');  //matchea el titulo
-//     })
-// })
--- wdio.conf.js
+++ wdio.conf.js
@@ -52,20 +52,25 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [{
-    
+    capabilities: [
+    //{
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        //maxInstances: 5,
         //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
+        //browserName: 'chrome',
+        //acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
-    }],
+    //},
+    {
+        browserName: 'firefox',
+        acceptInsecureCerts: true
+    }
+    ],
     //
     // ===================
     // Test Configurations
